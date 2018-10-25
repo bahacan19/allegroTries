6fa12e09db403ffd47934d312a0822c8c3d45b44
Baha Can Aydın <bahacan.aydin@akbank.com>
2018-10-25 17:55:46 +0300

    tryouts

--- removecolumn.c
+++ removecolumn.c
@@ -8,47 +8,49 @@ int MATRIX_HEIGHT = 5;
 
 size_t MATRIX_WIDHT = 5;
 
-void removeColumnA(int **matrix, int col);
+void removeColumnA(float **matrix, int col);
+
+void removeColumnMy(size_t rows, size_t columns, float matrix[rows][columns],int col);
 
 int main() {
-    int A[5][9];
-    int temp_gc;
+    float A[5][9];
+
+
     for (int i = 0; i < 5; ++i) {
         for (int j = 0; j < 9; ++j) {
             A[i][j] = 0;
         }
     }
-    printf("%d",temp_gc);
-    temp_gc = temp_gc - 10;
-    printf("%d",temp_gc);
-    A[0][8]  = temp_gc;
 
-    A[0][0] = 0;
-    A[1][0] = 0;
-    A[2][0] = 0;
-    A[3][0] = 0;
-    A[4][0] = 0;
+    //birinci düğüm
+    A[0][4] = 1;
+    A[0][5] = -1;
+    A[0][8] = -40;
 
-    A[0][1] = 0;
-    A[1][1] = 0;
-    A[2][1] = 0;
-    A[3][1] = 0;
-    A[4][1] = 0;
+    //ikinci düğüm
+
+    A[1][5] = 1;
+    A[1][8] = 50;
 
 
-    A[0][2] = 3;
-    A[1][2] = 4;
-    A[2][2] = 5;
-    A[3][2] = 6;
-    A[4][2] = 7;
+    //üçüncü düğüm
+
+    A[2][3] = 1;
+    A[2][8] = 80;
+
+    //dördüncü düğüm
+
+    A[3][4] = 1;
+    A[3][8] = 10;
+
+
+    //giriş - çıkış
+
+    A[4][3] = 1;
+    A[4][8] = 80;
 
 
 
-    A[0][3] = 3;
-    A[1][3] = 4;
-    A[2][3] = 5;
-    A[3][3] = 6;
-    A[4][3] = 7;
     //removeColumnA(A,0);
 
 
@@ -57,16 +59,105 @@ int main() {
 
     for (int i = 0; i < 5; ++i) {
         for (int j = 0; j < 9; ++j) {
-            printf("%d ",A[i][j]);
+            printf("%.2f  ",A[i][j]);
         }
         printf("\n");
     }
     printf("\n");
     printf("\n");
+    float *ip1 = &A;
+    
+    //removeColumnMy( 5,9 , A, 0);
+    //removeColumnA(ip1,0);
+
+    int q,w,k,n=5;
+    float c;
+    for(w=1; w<=8; w++)
+    {
+
+        for(q=1; q<=n; q++)
+        {
+            if(q!=w)
+            {
+                c=A[q][w]/A[w][w];
+                for(k=1; k<=n+1; k++)
+                {
+                    A[q][k]=A[q][k]-c*A[w][k];
+                }
+            }
+        }
+    }
+
+
+
+
+
+    /*printf("\n");
+    printf("\n");
+
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            printf("%.2f  ",A[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    printf("\n");*/
+
 
 }
 
-void removeColumnA(int **matrix, int col) {
+void removeColumnMy(size_t rows, size_t columns, float matrix[rows][columns], int colNum) {
+    MATRIX_WIDTH--;
+    float A[rows][columns-1];
+
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            A[i][j] = 0;
+        }
+    }
+    /*for (int i = 0; i < columns; ++i) {
+        for (int j = 0; j < rows; ++j) {
+            if (i == colNum) {
+                continue;
+            } else {
+                if (j == 0) {
+
+                } else {
+                    A[i][j] = matrix[i][j];
+                }
+            }
+        }
+    }*/
+    int temp = colNum;
+    //TODO check for empty matrix etc;
+    for (int i = 0; i < MATRIX_HEIGHT; i++) {
+        colNum = temp;
+        while (colNum < MATRIX_WIDTH) {
+            //move data to the left
+            A[i][colNum] = matrix[i][colNum + 1];
+            colNum++;
+        }
+        //matrix[i] = realloc(matrix[i], sizeof(double) * MATRIX_WIDHT);
+        //A[i] = matrix[i];
+        /*for (int j = 0; j < MATRIX_WIDHT; ++j) {
+            A[i][j] = matrix[i][j];
+        }*/
+    }
+    printf("\n");
+    printf("\n");
+
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            printf("%.2f  ",A[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    printf("\n");
+}
+
+void removeColumnA(float **matrix, int col) {
     MATRIX_WIDTH--;
     //TODO check for empty matrix etc;
     for (int i = 0; i < MATRIX_HEIGHT; i++) {
